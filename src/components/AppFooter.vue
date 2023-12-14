7ac1f5726a7494971e7d1bb3590938c4eeae57d9
Dylo <dylogic06@gmail.com>
2023-12-14 11:32:56 +0100

    comit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container-fluid p-0">
+        <p class="text-center">&copy;2023 - BG</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
